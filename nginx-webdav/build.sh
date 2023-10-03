4a926d4ea9574c0b757e570f095b2c1b8ab32955
jayson wang <jayson@laboys.org>
2023-10-03 23:47:45 +0800

    custom image name

--- nginx-webdav/build.sh
+++ nginx-webdav/build.sh
@@ -7,7 +7,7 @@ set -o pipefail
 WORKSPACE="$(cd -- "$(dirname "${BASH_SOURCE[0]}")" &>/dev/null && pwd -P)"
 
 WEBDAV_MODULE="webdav-ext"
-WEBDAV_IMAGE_NAME="wjiec/nginx-webdav:latest"
+WEBDAV_IMAGE_NAME=${WEBDAV_IMAGE_NAME:-"wjiec/nginx-webdav:latest"}
 
 curl -o "${WORKSPACE}/Dockerfile" "https://raw.githubusercontent.com/nginxinc/docker-nginx/master/modules/Dockerfile.alpine"
 
@@ -16,3 +16,4 @@ printf "libxml2-dev\nlibxslt-dev" > "${WORKSPACE}/${WEBDAV_MODULE}/build-deps"
 echo "https://github.com/arut/nginx-dav-ext-module/archive/master.tar.gz" > "${WORKSPACE}/${WEBDAV_MODULE}/source"
 
 docker build -t "${WEBDAV_IMAGE_NAME}" --build-arg="ENABLED_MODULES=${WEBDAV_MODULE}" "$@" .
+
